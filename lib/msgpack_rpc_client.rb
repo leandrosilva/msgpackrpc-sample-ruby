da091fc577d435d656c75526bea55927b5b57ab0
Leandro Silva <leandrodoze@gmail.com>
2012-06-07 20:51:51 -0300

    refactored

--- lib/msgpack_rpc_client.rb
+++
@@ -1,7 +0,0 @@
-require 'msgpack/rpc'
-
-c = MessagePack::RPC::Client.new('127.0.0.1', 1978)
-puts "10 + 2 = #{c.call(:add, 10, 2)}"
-puts "10 - 2 = #{c.call(:sub, 10, 2)}"
-puts "10 * 2 = #{c.call(:mul, 10, 2)}"
-puts "10 / 2 = #{c.call(:div, 10, 2)}"
